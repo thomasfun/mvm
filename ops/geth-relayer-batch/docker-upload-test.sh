52d4b548d4ab0afefd916d1d719d0aaf59225602
guanjie <jguanisme@163.com>
2021-10-17 03:15:22 +0000

    geth-relayer-batch reset

---
+++ ops/geth-relayer-batch/docker-upload-test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -ex
+region='us-east-2'
+if [ -n "$1"  ]
+then
+    region=$1
+fi
+# --no-cache
+echo 'Building metis_l2_geth image'
+cp ./efs-utils.conf ./settings/efs-utils.conf
+cmd="sed -i s#REGION_VAR_FOR_ENV#$region#g ./settings/efs-utils.conf"
+$cmd
+# docker images|grep metis_l2_geth|awk '{print $3}'|xargs docker rmi -f
+docker build --no-cache -f ./Dockerfile -t metis_l2_geth ../geth-relayer-batch
+cmd="sed -i s#$region#REGION_VAR_FOR_ENV#g ./settings/efs-utils.conf"
+$cmd
+
+profile="aws --profile default ecr get-login-password --region $region"
+login="docker login --username AWS --password-stdin 950087689901.dkr.ecr.$region.amazonaws.com"
+$profile | $login
+
+echo 'Pushing metis_l2_geth'
+l2geth="docker tag metis_l2_geth:latest 950087689901.dkr.ecr.$region.amazonaws.com/metis-l2-geth:latest"
+$l2geth
+l2geth_push="docker push  950087689901.dkr.ecr.$region.amazonaws.com/metis-l2-geth:latest"
+$l2geth_push
+
+echo 'Pushing data-transport-layer'
+dtl="docker tag ethereumoptimism/data-transport-layer:latest 950087689901.dkr.ecr.$region.amazonaws.com/metis-dtl:latest"
+$dtl
+dtl_push="docker push 950087689901.dkr.ecr.$region.amazonaws.com/metis-dtl:latest"
+$dtl_push
+
--- ops/geth-relayer-batch/settings/process_kill.sh
+++ ops/geth-relayer-batch/settings/process_kill.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-ps -ef | grep process_monitor.sh | grep -v grep | awk '{print $2}'|xargs kill -9
-ps -ef | grep geth | grep verbosity | grep -v grep | awk '{print $2}'|xargs kill -9
-ps -ef | grep run-batch-submitter.js | grep -v grep | awk '{print $2}'|xargs kill -9
-ps -ef | grep run-message-relayer.js | grep -v grep | awk '{print $2}'|xargs kill -9
+if [[ $# -gt 0 ]]; then
+    echo "kill process_monitor">>/app/log/t_supervisord.log
+    ps -ef | grep process_monitor.sh | grep -v grep | awk '{print $2}'|xargs kill -9
+    case "$1" in
+    "geth")
+        ps -ef | grep geth | grep verbosity | grep -v grep | awk '{print $2}'|xargs kill -9
+        ;;
+    "batch-submitter")
+        ps -ef | grep run-batch-submitter.js | grep -v grep | awk '{print $2}'|xargs kill -9
+        ;;
+    "message-relayer")
+        ps -ef | grep run-message-relayer.js | grep -v grep | awk '{print $2}'|xargs kill -9
+        ;;
+    *)
+        echo "$* is illegal">>/app/log/t_supervisord.log
+        ;;
+    esac
+    echo "restart process_monitor">>/app/log/t_supervisord.log
+    nohup /app/process_monitor.sh >>/app/log/t_supervisord.log 2>&1 &
+else
+    ps -ef | grep process_monitor.sh | grep -v grep | awk '{print $2}'|xargs kill -9
+    ps -ef | grep geth | grep verbosity | grep -v grep | awk '{print $2}'|xargs kill -9
+    ps -ef | grep run-batch-submitter.js | grep -v grep | awk '{print $2}'|xargs kill -9
+    ps -ef | grep run-message-relayer.js | grep -v grep | awk '{print $2}'|xargs kill -9
+fi
